b0ac1f50648723173c6f15ac91baf4ebef7d45b0
Mario <mflorence1989@gmail.com>
2018-04-11 23:54:46 -0400

    implement the page layout for details

--- assets/js/jobs/pages/Details.js
+++ assets/js/jobs/pages/Details.js
@@ -1,25 +1,72 @@
-import React, { Component} from 'react'
-import ReactDOM from 'react-dom'
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
 export default class Details extends Component {
-  constructor () {
-    super()
-    this.state = {
-
-    }
+  constructor() {
+    super();
+    this.state = {};
   }
 
-
-
-  render () {
-    const {match, location, history} = this.props
-      return(
-        <div className="details-page">
+  render() {
+    const { match, location, history } = this.props;
+    return (
+      <div className="details-page">
         <div className="container">
-      this item is {match.params.item}
+          <section className="submenu">
+            <div className="direction">
+              <div className="prev">Prev</div>
+              <div className="next">NEXT</div>
+            </div>
+            <nav className="sub-links">
+              <a href="#">More ads by user</a>
+              <a href="#">Print</a>
+              <a href="#">Share</a>
+              <a href="#">contact seller</a>
+            </nav>
+          </section>
+          <section>
+            <div className="media-column">
+              <div className="gallery">
+                <div className="slider">
+                  <div className="main-image">
+                    <div className="left-arrow">
+                      <i className="fas fa-angle-left" />
+                    </div>
+                    <div className="right-arrow">
+                      <i className="fas fa-angle-right" />
+                    </div>
+                  </div>
+                </div>
+                <div className="thumbnails">
+                  <div className="thumb-img">image</div>
+                </div>
+              </div>
+            </div>
+            <div className="details-column">
+              <div className="date">4/19/1989</div>
+              <h3>porshe</h3>
+              <h4 className="price">1344</h4>
+              <div className="more-details">
+                <div className="info">
+                  <label>Vin</label>
+                  <h5>used </h5>
+                </div>
+                <div className="info">
+                  <label>Mileage</label>
+                  <h5>used </h5>
+                </div>
+                <div className="info">
+                  <label>Vin</label>
+                  <h5>used </h5>
+                </div>
+              </div>
+              <div className="description">
+                <p>this is where the description goes </p>
+              </div>
+            </div>
+          </section>
         </div>
-        </div>
-      )
-
+      </div>
+    );
   }
 }
--- public/js/components/jobs.js
+++ public/js/components/jobs.js
@@ -329,7 +329,7 @@ var Details = function (_Component) {
   }
 
   _createClass(Details, [{
-    key: 'render',
+    key: "render",
     value: function render() {
       var _props = this.props,
           match = _props.match,
@@ -337,13 +337,166 @@ var Details = function (_Component) {
           history = _props.history;
 
       return _react2.default.createElement(
-        'div',
-        { className: 'details-page' },
+        "div",
+        { className: "details-page" },
         _react2.default.createElement(
-          'div',
-          { className: 'container' },
-          'this item is ',
-          match.params.item
+          "div",
+          { className: "container" },
+          _react2.default.createElement(
+            "section",
+            { className: "submenu" },
+            _react2.default.createElement(
+              "div",
+              { className: "direction" },
+              _react2.default.createElement(
+                "div",
+                { className: "prev" },
+                "Prev"
+              ),
+              _react2.default.createElement(
+                "div",
+                { className: "next" },
+                "NEXT"
+              )
+            ),
+            _react2.default.createElement(
+              "nav",
+              { className: "sub-links" },
+              _react2.default.createElement(
+                "a",
+                { href: "#" },
+                "More ads by user"
+              ),
+              _react2.default.createElement(
+                "a",
+                { href: "#" },
+                "Print"
+              ),
+              _react2.default.createElement(
+                "a",
+                { href: "#" },
+                "Share"
+              ),
+              _react2.default.createElement(
+                "a",
+                { href: "#" },
+                "contact seller"
+              )
+            )
+          ),
+          _react2.default.createElement(
+            "section",
+            null,
+            _react2.default.createElement(
+              "div",
+              { className: "media-column" },
+              _react2.default.createElement(
+                "div",
+                { className: "gallery" },
+                _react2.default.createElement(
+                  "div",
+                  { className: "slider" },
+                  _react2.default.createElement(
+                    "div",
+                    { className: "main-image" },
+                    _react2.default.createElement(
+                      "div",
+                      { className: "left-arrow" },
+                      _react2.default.createElement("i", { className: "fas fa-angle-left" })
+                    ),
+                    _react2.default.createElement(
+                      "div",
+                      { className: "right-arrow" },
+                      _react2.default.createElement("i", { className: "fas fa-angle-right" })
+                    )
+                  )
+                ),
+                _react2.default.createElement(
+                  "div",
+                  { className: "thumbnails" },
+                  _react2.default.createElement(
+                    "div",
+                    { className: "thumb-img" },
+                    "image"
+                  )
+                )
+              )
+            ),
+            _react2.default.createElement(
+              "div",
+              { className: "details-column" },
+              _react2.default.createElement(
+                "div",
+                { className: "date" },
+                "4/19/1989"
+              ),
+              _react2.default.createElement(
+                "h3",
+                null,
+                "porshe"
+              ),
+              _react2.default.createElement(
+                "h4",
+                { className: "price" },
+                "1344"
+              ),
+              _react2.default.createElement(
+                "div",
+                { className: "more-details" },
+                _react2.default.createElement(
+                  "div",
+                  { className: "info" },
+                  _react2.default.createElement(
+                    "label",
+                    null,
+                    "Vin"
+                  ),
+                  _react2.default.createElement(
+                    "h5",
+                    null,
+                    "used "
+                  )
+                ),
+                _react2.default.createElement(
+                  "div",
+                  { className: "info" },
+                  _react2.default.createElement(
+                    "label",
+                    null,
+                    "Mileage"
+                  ),
+                  _react2.default.createElement(
+                    "h5",
+                    null,
+                    "used "
+                  )
+                ),
+                _react2.default.createElement(
+                  "div",
+                  { className: "info" },
+                  _react2.default.createElement(
+                    "label",
+                    null,
+                    "Vin"
+                  ),
+                  _react2.default.createElement(
+                    "h5",
+                    null,
+                    "used "
+                  )
+                )
+              ),
+              _react2.default.createElement(
+                "div",
+                { className: "description" },
+                _react2.default.createElement(
+                  "p",
+                  null,
+                  "this is where the description goes "
+                )
+              )
+            )
+          )
         )
       );
     }
