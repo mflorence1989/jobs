b745613c87c046506fb663c65d9f4e25df3f9c5e
Mario <mflorence1989@gmail.com>
2018-04-13 12:49:53 -0400

    implement data for categories

---
+++ data/catgegories.js
@@ -0,0 +1,393 @@
+module.exports = [
+  {
+    title: "community",
+    listings: [
+      {
+        name: 'activities',
+        slug: 'activities',
+
+      },
+      {
+        name: 'artists',
+        slug: 'artists',
+
+      },
+      {
+        name: 'childcare',
+        slug: 'childcare',
+
+      },
+      {
+        name: 'activities',
+        slug: 'activities',
+
+      },
+      {
+        name: 'activities',
+        slug: 'activities',
+
+      },
+      {
+        name: 'classes',
+        slug: 'classes',
+
+      },
+      {
+        name: 'events',
+        slug: 'events',
+
+      },
+      {
+        name: 'general',
+        slug: 'general',
+
+      },
+      {
+        name: 'groups',
+        slug: 'groups',
+
+      },
+      {
+        name: 'local news',
+        slug: 'local-news',
+
+      }
+    ]
+  },
+  {
+    title: "housing",
+    listings: [
+      {
+        name: 'apartment',
+        slug: 'apartment',
+
+      },
+      {
+        name: 'housing swap',
+        slug: 'housing-swap',
+
+      },
+      {
+        name: 'office',
+        slug: 'office',
+
+      },
+      {
+        name: 'parking',
+        slug: 'parking',
+
+      },
+      {
+        name: 'real estate for sale',
+        slug: 'real-estate-for-sale',
+
+      },
+      {
+        name: 'rooms',
+        slug: 'rooms',
+
+      },
+      {
+        name: 'vacation rental',
+        slug: 'vacation-rental',
+
+      },
+      {
+        name: 'sublets',
+        slug: 'sublets',
+
+      },
+      {
+        name: 'storgae',
+        slug: 'storage',
+
+      },
+      {
+        name: 'land',
+        slug: 'land',
+
+      }
+    ]
+  },
+  {
+    title: "services",
+    listings: [
+      {
+        name: 'automotive',
+        slug: 'automotive',
+
+      },
+      {
+        name: 'beauty',
+        slug: 'beauty',
+
+      },
+      {
+        name: 'cell phone',
+        slug: 'cell-phone',
+
+      },
+      {
+        name: 'computer',
+        slug: 'computer',
+
+      },
+      {
+        name: 'creative',
+        slug: 'creative',
+
+      },
+      {
+        name: 'cycle',
+        slug: 'cycle',
+
+      },
+      {
+        name: 'legal',
+        slug: 'legal',
+
+      },
+      {
+        name: 'pet',
+        slug: 'pet',
+
+      },
+      {
+        name: 'household',
+        slug: 'household',
+
+      },
+      {
+        name: 'ads',
+        slug: 'ads',
+
+      }
+    ]
+  },
+  {
+    title: "discussion forum",
+    listings: [
+      {
+        name: 'apple',
+        slug: 'apple',
+
+      },
+      {
+        name: 'arts',
+        slug: 'arts',
+
+      },
+      {
+        name: 'auto',
+        slug: 'auto',
+
+      },
+      {
+        name: 'help',
+        slug: 'help',
+
+      },
+      {
+        name: 'history',
+        slug: 'history',
+
+      },
+      {
+        name: 'general',
+        slug: 'general',
+
+      },
+      {
+        name: 'legal',
+        slug: 'legal',
+
+      },
+      {
+        name: 'free',
+        slug: 'free',
+
+      },
+      {
+        name: 'furniture',
+        slug: 'furniture',
+
+      },
+      {
+        name: 'jobs',
+        slug: 'jobs',
+
+      }
+    ]
+  },
+  {
+    title: "for-sale",
+    listings: [
+      {
+        name: 'antiques',
+        slug: 'antiques',
+
+      },
+      {
+        name: 'art',
+        slug: 'art',
+
+      },
+      {
+        name: 'auto',
+        slug: 'auto',
+
+      },
+      {
+        name: 'books',
+        slug: 'books',
+
+      },
+      {
+        name: 'tickets',
+        slug: 'tickets',
+
+      },
+      {
+        name: 'general',
+        slug: 'general',
+
+      },
+      {
+        name: 'jewelery',
+        slug: 'jewelery',
+
+      },
+      {
+        name: 'bike',
+        slug: 'bike',
+
+      },
+      {
+        name: 'furniture',
+        slug: 'furniture',
+
+      },
+      {
+        name: 'cars+trucks',
+        slug: 'cars-and-trucks',
+
+      }
+    ]
+  },
+  {
+    title: "jobs",
+    listings: [
+      {
+        name: 'accounting',
+        slug: 'accounting',
+
+      },
+      {
+        name: 'web developer',
+        slug: 'web-developer',
+
+      },
+      {
+        name: 'auto mechanic',
+        slug: 'auto-mechanic',
+
+      },
+      {
+        name: 'general labor',
+        slug: 'general-labor',
+
+      },
+      {
+        name: 'education',
+        slug: 'education',
+
+      },
+      {
+        name: 'freelance',
+        slug: 'freelance',
+
+      },
+      {
+        name: 'jeweler',
+        slug: 'jeweler',
+
+      },
+      {
+        name: 'tech',
+        slug: 'tech',
+
+      },
+      {
+        name: 'sales',
+        slug: 'sales',
+
+      },
+      {
+        name: 'janitor',
+        slug: 'janitor',
+
+      }
+    ]
+  },
+  {
+    title: "gigs",
+    listings: [
+      {
+        name: 'computer',
+        slug: 'computer',
+
+      },
+      {
+        name: 'event',
+        slug: 'event',
+
+      },
+      {
+        name: 'creative',
+        slug: 'creative',
+
+      },
+      {
+        name: 'domestic',
+        slug: 'domestic',
+
+      },
+      {
+        name: 'labor',
+        slug: 'labor',
+
+      },
+      {
+        name: 'talent',
+        slug: 'writing',
+
+      },
+      {
+        name: 'crew',
+        slug: 'crew',
+
+      },
+      {
+        name: 'rock show',
+        slug: 'rock-show',
+
+      },
+      {
+        name: 'hip hop show',
+        slug: 'hip-hop-show',
+
+      },
+      {
+        name: 'circus',
+        slug: 'circus',
+
+      }
+    ]
+  },
+  {
+    title: "resumes",
+    listings: [
+
+    ]
+  },
+]
